81fd81f817d0b892347e1735e27b07dd45cf2550
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-10-31 14:23:47 +0100

    change clear color

--- include/ve/vulkan/command_buffer.hpp
+++ include/ve/vulkan/command_buffer.hpp
@@ -190,7 +190,7 @@ namespace vulkan {
 					// color
 					vk::clear_value{
 						.color = vk::clear_color_value{
-							.float32 = {0.04f, 0.04f, 0.04f, 1.0f}
+							.float32 = {0.02f, 0.02f, 0.03f, 1.0f}
 						}
 					},
 					// depth
@@ -408,10 +408,11 @@ namespace vulkan {
 						// pipeline layout
 						pipeline.layout(),
 						// stage flags
-						VK_SHADER_STAGE_VERTEX_BIT
-						//| VK_SHADER_STAGE_FRAGMENT_BIT
-						| VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT
-						| VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT
+						//VK_SHADER_STAGE_VERTEX_BIT
+						//|
+						VK_SHADER_STAGE_FRAGMENT_BIT
+						//| VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT
+						//| VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT
 						,
 						// offset
 						0U,
