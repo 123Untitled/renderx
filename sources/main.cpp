8e634a93e8fae9db1301c0df836c67f1a37c0be7
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-11-06 12:41:23 +0000

    update

--- sources/main.cpp
+++ sources/main.cpp
@@ -10,6 +10,29 @@
 
 auto main(int, char**) -> int {
 
+	// wayland
+	{
+		wl::display display;
+		wl::compositor compositor{display};
+		wl::surface surface{compositor};
+
+		//create_vulkan_surface(display, surface);
+
+		surface.commit();
+
+		display.roundtrip();
+
+		std::cout << "Wayland display: " << &display.get() << std::endl;
+		while (display.dispatch() != 0U) {
+			// do nothing
+		}
+
+		sleep(1);
+
+		return 0;
+	}
+
+	return 0;
 
 	ve::mesh::skybox skybox;
 
